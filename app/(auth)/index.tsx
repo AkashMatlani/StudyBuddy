4a7a150a8be19557c494cbcd1bc1268771fdb356
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-04-24 12:16:16 +0100

    Linear Gardiant added

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,5 +1,25 @@
-import { Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { View } from "react-native";
+import useSocialAuth from "../hooks/useSocialAuth";
 
 export default function AuthScreen() {
-    return (<Text>Auth Screen</Text>)
+
+    const { handleSocailAuth, loadingStartagy } = useSocialAuth();
+
+    return (
+        <View className="flex-1 bg-background">
+            {/* Gradient Background */}
+            <View className="absolute inset-0">
+                <LinearGradient colors={[
+                    "#0F0E17", "#1A1A2E", "#2D1B69", "#1A1A2E", "#0F0E17",
+                ]}
+                    locations={[0, 0.25, 0.5, 0.75, 1]}
+                    style={{ width: "100%", height: "100%" }}
+                    start={{ x: 0.5, y: 0 }}
+                    end={{ x: 0.5, y: 1 }}
+                >
+                </LinearGradient>
+            </View>
+        </View>
+    )
 }
